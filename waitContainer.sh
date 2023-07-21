2341ca7038db8608a237d1b551845da3132be527
BraisCabo <braiscabofelpete@gmail.com>
2023-07-21 17:30:24 +0200

    cicd

--- waitContainer.sh
+++ waitContainer.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+MAX_WAIT_SECONDS=600
+
+elapsed_seconds=0
+
 while ! nc -z localhost 8443; do
   printf "\n  => Waiting for application to start - IP: http://localhost and PORT: '8443'"
   sleep 1s
+  ((elapsed_seconds++))
+
+  if [ "$elapsed_seconds" -ge "$MAX_WAIT_SECONDS" ]; then
+    printf "\n  => Application took too long to start. Exiting...\n"
+    exit 1
+  fi
 done
