e7d42e0042da224fc3487e16df780e468ef8d8aa
BraisCabo <braiscabofelpete@gmail.com>
2023-07-21 17:20:40 +0200

    cicd

--- waitContainer.sh
+++ waitContainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while ! nc -z "http://localhost:8443"; do
+while ! nc -z "http://localhost" 8443; do
   printf "\n  => Waiting for application to start - IP: http://localhost and PORT: '8443'"
   sleep 1s
 done
